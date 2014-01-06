da1899ce27248d54f1fe145376a1867b90df5e83
unknown <579738mgr@043118430753.na.odcorp.net>
2014-01-06 11:15:52 -0500

    first commit

---
+++ test/clojure/projecteuler/net_test.clj
@@ -0,0 +1,7 @@
+(ns clojure.projecteuler.net-test
+  (:require [clojure.test :refer :all]
+            [clojure.projecteuler.net :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
